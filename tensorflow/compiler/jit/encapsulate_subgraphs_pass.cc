6f0f50ed29edb5edf9d208409eb4dc3c49bf0465
Utku Saglam <utku.saglam@h-partners.com>
2026-03-11 22:34:56 +0800

    log cluster

--- tensorflow/compiler/jit/encapsulate_subgraphs_pass.cc
+++ tensorflow/compiler/jit/encapsulate_subgraphs_pass.cc
@@ -672,8 +672,20 @@ absl::Status Encapsulator::Subgraph::RecordArg(
       const TensorShapeProto& shape = shape_attr->list().shape(src_slot);
       TensorShapeProto* tsp =
           mutable_shape_attr.mutable_list()->mutable_shape(src_slot);
-      LOG(INFO) << "Found shape attribute in " << src_node->name() << ":"
-                << expr_map[src_node->name()][src_slot]->DebugString();
+        
+        const auto& exprs = expr_map[src_node->name()][src_slot];
+        LOG(INFO) << "Found shape attribute in " << src_node->name()
+                  << ":" << src_slot
+                  << " expr_count=" << exprs.size();
+
+        for (int dim_idx = 0; dim_idx < static_cast<int>(exprs.size()); ++dim_idx) {
+          const auto& expr = exprs[dim_idx];
+          if (!expr) {
+            LOG(INFO) << "  dim[" << dim_idx << "]: <null>";
+            continue;
+          }
+          LOG(INFO) << "  dim[" << dim_idx << "]: " << expr->DebugString();
+        }
       std::vector<std::unique_ptr<DimExpr>> expressions =
           std::move(expr_map[src_node->name()][src_slot]);
 
