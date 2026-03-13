c3380206c1f2a82328e0c0e85855c56c81b30184
Utku Saglam <utku.saglam@h-partners.com>
2026-03-13 19:00:37 +0800

    reshape inference reverted

--- tensorflow/core/ops/array_ops.cc
+++ tensorflow/core/ops/array_ops.cc
@@ -210,11 +210,6 @@ absl::Status SetOutputShapeForReshape(InferenceContext* c) {
       for (int32_t i = 0; i < c->Rank(out); ++i) {
         DimensionHandle dim = c->Dim(out, i);
         if (!c->ValueKnown(dim)) {
-          if (c->GetDimExpr(dim) != nullptr) {
-            TF_RETURN_IF_ERROR(
-                c->Multiply(known_out_elems, dim, &known_out_elems));
-            continue;
-          }
           if (out_unknown_idx >= 0) {
             too_many_unknown = true;
             break;
@@ -233,11 +228,6 @@ absl::Status SetOutputShapeForReshape(InferenceContext* c) {
       for (int32_t i = 0; i < c->Rank(in); ++i) {
         DimensionHandle dim = c->Dim(in, i);
         if (!c->ValueKnown(dim)) {
-          if (c->GetDimExpr(dim) != nullptr) {
-            TF_RETURN_IF_ERROR(
-                c->Multiply(known_in_elems, dim, &known_in_elems));
-            continue;
-          }
           if (in_unknown_idx >= 0) {
             too_many_unknown = true;
             break;
