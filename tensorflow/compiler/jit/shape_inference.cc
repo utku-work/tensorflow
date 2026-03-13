e37cc93b58bd1c0b2534274a7167487534a76c4c
Utku Saglam <utku.saglam@h-partners.com>
2026-03-13 17:48:12 +0800

    shape_inference

--- tensorflow/compiler/jit/shape_inference.cc
+++ tensorflow/compiler/jit/shape_inference.cc
@@ -49,17 +49,10 @@ absl::Status ShapeHandleToTensorShape(
   if (!context->RankKnown(handle)) return absl::OkStatus();
 
   std::vector<int64_t> dims(context->Rank(handle));
-  std::vector<xla::DynExpr*> dyn_exprs(context->Rank(handle));
   for (int32_t i = 0, end = dims.size(); i < end; ++i) {
     dims[i] = context->Value(context->Dim(handle, i));
-    auto ratio = context->DynamicRatio(context->Dim(handle, i));
-    dyn_exprs[i] = ratio > 0 ? (ratio * *xla::DynExpr::V(1))->s()
-                             : xla::DynExpr::_(dims[i]);  // For now
   }
-  auto status =
-      PartialTensorShape::MakePartialShape(dims.data(), dims.size(), shape);
-  shape->set_expressions(dyn_exprs);
-  return status;
+  return PartialTensorShape::MakePartialShape(dims.data(), dims.size(), shape);
 }
 
 absl::Status PropagateShapes(
