a55f7257086077deffd5ccee6b37b49a669ee9a3
Utku Saglam <utku.saglam@h-partners.com>
2026-03-21 19:40:11 +0800

    Env pushed.

--- tensorflow/core/framework/tensor_shape.cc
+++ tensorflow/core/framework/tensor_shape.cc
@@ -15,6 +15,7 @@ limitations under the License.
 
 #include "tensorflow/core/framework/tensor_shape.h"
 
+#include "absl/strings/ascii.h"
 #include "tensorflow/core/framework/bounds_check.h"
 #include "tensorflow/core/framework/tensor_shape.pb.h"
 #include "tensorflow/core/lib/strings/str_util.h"
@@ -22,19 +23,32 @@ limitations under the License.
 #include "tensorflow/core/platform/errors.h"
 #include "tensorflow/core/platform/logging.h"
 #include "tensorflow/core/platform/macros.h"
+#include "tensorflow/core/util/env_var.h"
 #include "tensorflow/core/util/overflow.h"
-#include "xla/parse_flags_from_env.h"
 
 namespace tensorflow {
 
 namespace {
 
+bool EnvFlagContains(absl::string_view flags, absl::string_view name) {
+  size_t pos = 0;
+  while ((pos = flags.find(name, pos)) != absl::string_view::npos) {
+    const bool start_ok = pos == 0 || absl::ascii_isspace(flags[pos - 1]);
+    const size_t end = pos + name.size();
+    const bool end_ok = end == flags.size() || absl::ascii_isspace(flags[end]) ||
+                        flags[end] == '=';
+    if (start_ok && end_ok) {
+      return true;
+    }
+    pos = end;
+  }
+  return false;
+}
+
 const bool kTensorShapeExpressionsEnabled = [] {
-  bool enable_dynamic_sizes = false;
-  std::vector<tsl::Flag> flag_list = {
-      tsl::Flag("tf_xla_enable_dynamic_sizes", &enable_dynamic_sizes, "")};
-  xla::ParseFlagsFromEnvAndIgnoreUnknown("TF_XLA_FLAGS", flag_list);
-  return enable_dynamic_sizes;
+  string tf_xla_flags;
+  TF_CHECK_OK(ReadStringFromEnvVar("TF_XLA_FLAGS", "", &tf_xla_flags));
+  return EnvFlagContains(tf_xla_flags, "--tf_xla_enable_dynamic_sizes");
 }();
 
 }  // namespace
